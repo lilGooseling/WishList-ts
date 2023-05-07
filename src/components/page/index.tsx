fc66d7039ca4e0363c2af5d35e3ce5e08bbc1544
Наталья Оскарова <gooseberry.bay@gmail.com>
2023-05-07 20:33:52 +0300

    BackgroundEffect page

--- src/components/page/index.tsx
+++ src/components/page/index.tsx
@@ -7,6 +7,7 @@ import Section from "../section";
 import Container from "../container";
 import Color from "../backgroundColor";
 import BackgroundImage from "../backgroundImage";
+import BackgroundEffect from "../backgroundEffect";
 import {globalData} from "../../data";
 
 
@@ -18,25 +19,27 @@ const Page: React.FC<PageProps> = ({page}) => {
     const pageType = page.type || PageTypeEnum.LEFT;
 
     return  <Color backgroundColor={page.backgroundColor}>
-            <BackgroundImage backgroundImage={page.backgroundImage} imageTemplate={page.imageTemplate}>
-                <Container fullScreen={page.fullScreen}>
-                    <div className={styles.site_title}>
-                        {globalData.title}
-                    </div>
-                    <div className={styles.site_description}>
-                        {globalData.description}
-                    </div>
-                    <Menu/>
-                    <div className={classNames(styles.title, {
-                        [styles.left]: pageType === PageTypeEnum.LEFT,
-                        [styles.right]: pageType === PageTypeEnum.RIGHT,
-                        [styles.center]: pageType === PageTypeEnum.CENTER,
-                    })}>
-                        {page.title}
-                    </div>
-                </Container>
-                {page.sections.map(section => <Section section={section} key={section.title}/>)}
-            </BackgroundImage>
+            <BackgroundEffect backgroundEffect={page.backgroundEffect} >
+                <BackgroundImage backgroundImage={page.backgroundImage} imageTemplate={page.imageTemplate}>
+                    <Container fullScreen={page.fullScreen}>
+                        <div className={styles.site_title}>
+                            {globalData.title}
+                        </div>
+                        <div className={styles.site_description}>
+                            {globalData.description}
+                        </div>
+                        <Menu/>
+                        <div className={classNames(styles.title, {
+                            [styles.left]: pageType === PageTypeEnum.LEFT,
+                            [styles.right]: pageType === PageTypeEnum.RIGHT,
+                            [styles.center]: pageType === PageTypeEnum.CENTER,
+                        })}>
+                            {page.title}
+                        </div>
+                    </Container>
+                    {page.sections.map(section => <Section section={section} key={section.title}/>)}
+                </BackgroundImage>
+            </BackgroundEffect>
         </Color>
 
 };
